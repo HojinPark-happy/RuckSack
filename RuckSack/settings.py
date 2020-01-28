48aa1d8542252722c6961fdea43a3dca8011a51b
Erin Beachkofski <beachko2@uwm.edu>
2020-01-28 13:32:17 -0600

    Made html and css for homepage

--- RuckSack/settings.py
+++ RuckSack/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'RuckSackApp.apps.RucksackappConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- RuckSackApp/views.py
+++ RuckSackApp/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
-# Create your views here.
 
+####################### API URLS ######################
+teleportRootUrl = "https://api.teleport.org/api/"
+continentUrl = "https://api.teleport.org/api/continents/"
+countryUrl = "https://api.teleport.org/api/countries/"
+cityUrl = "https://api.teleport.org/api/cities/?search="
+#######################################################
 
 #
 def view_home_page(request):
-    return render(request, 'home_page.html')
+    continentResponse = requests.get(continentUrl)
+    continentData = continentResponse.json()
+    countryResponse = requests.get(countryUrl)
+    countryData = countryResponse.json()
+    cityResponse = requests.get(cityUrl)
+    cityData = cityResponse.json()
+    context = [
+        continentData,
+        countryData,
+        cityData]
+    return render(request, 'home_page.html', {'context': context})
 
