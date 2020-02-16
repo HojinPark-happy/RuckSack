cf06e56885aae31a5cc3e9aa9bde6e59cecc3500
Erin Beachkofski <beachko2@uwm.edu>
2020-02-16 15:29:09 -0600

    Did stuff with API and added svg logo

--- static/js/startPage.js
+++ static/js/startPage.js
@@ -1,23 +1,31 @@
-$(document).ready(function() {
-  $.getJSON("https://api.teleport.org/api/continents/", function( data ) {
 
-    //  Get and set the contents of the continent checkboxes
-    continentData = data['_links']['continent:items'];
-    setContinents(continentData);
+var input = document.getElementById('city_search');
+var options = { types: ['(cities)'] };
+var autocomplete;
 
-
-  });
-})
-
-
-//  METHOD: Sets the contents of the continent checkboxes
-//  PARAMS: continentData -> array of JSON data consisting of continents
-function setContinents(continentData) {
-  continentData.forEach( function (obj){
-        $('#continentList').append($('<option></option>').attr('value', obj.name).text(obj.name));
-    });
-
+function initialize(){
+    autocomplete = new google.maps.places.Autocomplete(input, options);
 }
+
+autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+        });
 
 
 
