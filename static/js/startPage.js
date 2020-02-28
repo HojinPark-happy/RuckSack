e185e22655bbd924aea2f74a8cb8ee1cdb9702df
Erin Beachkofski <beachko2@uwm.edu>
2020-02-28 11:23:14 -0600

    Only cities searchable

--- static/js/startPage.js
+++ static/js/startPage.js
@@ -1,6 +1,11 @@
 function initialize() {
+   var options = {
+    types: ['(cities)']
+   };
+
   var input = document.getElementById('city_search');
-  new google.maps.places.Autocomplete(input);
+  new google.maps.places.Autocomplete(input, options);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
